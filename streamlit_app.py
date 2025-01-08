fdf6b701f3c62183ca525b79d996323a8351d71a
stefm6 <stefan.mihailescu@blankfactor.com>
2025-01-08 13:47:37 +0200

    pd

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+from snowflake.snowpark import Session
 import requests
 import pandas as pd
 
@@ -14,9 +15,10 @@ st.write(
 name_on_order = st.text_input("Name on Smoothie")
 st.write("The name on your smoothie will be:", name_on_order)
 
-cnx = st.connection("snowflake")
-session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
+# cnx = st.connection("snowflake")
+# session = cnx.session()
+# my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
+my_dataframe = Session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 # st.stop()
 pd_df = my_dataframe.to_pandas()
