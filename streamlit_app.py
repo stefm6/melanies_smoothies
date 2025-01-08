65099a8b253f3f594effd388c83ddf1dea903a81
stefm6 <stefan.mihailescu@blankfactor.com>
2025-01-08 13:51:12 +0200

    pd

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,8 +15,8 @@ st.write(
 name_on_order = st.text_input("Name on Smoothie")
 st.write("The name on your smoothie will be:", name_on_order)
 
-# cnx = st.connection("snowflake")
-# session = cnx.session()
+cnx = st.connection("snowflake")
+session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 # st.stop()
